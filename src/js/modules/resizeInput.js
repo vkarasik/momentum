30fa64651f012f0c0dde2b3a0d1cd5bc0b7f6ed7
Vadim Karasik <vadkarasik@gmail.com>
2022-04-11 13:04:05 +0300

    deploy

--- src/js/modules/resizeInput.js
+++
@@ -1,9 +0,0 @@
-export default function resizeInput() {
-  const nameInput = document.querySelector('.greeting__name');
-  const fakeInput = document.createElement('span');
-  fakeInput.classList.add('greeting__fake-name');
-  fakeInput.textContent = nameInput.value;
-  nameInput.parentNode.appendChild(fakeInput);
-  nameInput.style.width = `${fakeInput.clientWidth}px`;
-  fakeInput.remove();
-}
--- src/js/modules/setName.js
+++
@@ -1,8 +0,0 @@
-function setName() {
-  const inputName = document.querySelector('.greeting__name');
-  const userName = localStorage.getItem('user-name');
-  inputName.style.width = 0;
-  inputName.value = userName ? userName : '[Enter Name]';
-}
-
-export default setName;
